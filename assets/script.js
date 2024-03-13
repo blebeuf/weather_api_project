fd20c64ee624062272a9397b57742d952e06077b
blebeuf <bryanlebeuf@gmail.com>
2024-03-12 22:28:35 -0500

    fix console.log issue and update error handler

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,32 @@
 const key = "11701a22eb50e7dd286864fa0017542d";
 const userInputEl = $('#user-input');
-const recentSearch = "";
-const btnEl = $('#search-button');
 
 // error handler for the fetch
-function currentWeather (event) {
-    event.preventDefault();
+$(document).ready(function() {
+// reference to button id in the HTML
+    $('#search-button').click(function() {
+        const userInput = userInputEl.val();
+        // Add "&units=imperial" to get the temperature in Fahrenheit
+        // here's a link to assist with switching units: https://openweather.my.site.com/s/article/switching-between-temperature-units-2019-10-24-21-47-24
+        const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}&units=imperial`;
 
-    const userInput = userInputEl.val();
-    const requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=${key}`;
+        
+// error handler for the fetch
+        fetch(requestUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => displayWeather(data))
+            .catch(error => {
+                console.error('There has been a problem with your fetch operation:', error);
+                $('#weather-info').html('<p>Failed to retrieve data. Please try again.</p>');
+            });
+    });
+});
 
-    console.log(userInput);
-    console.log(requestUrl);
-
-    fetch(requestUrl)
-    .then(function (response) {
-        if (response.ok) {
-            response.json()
-            .then(function(data) {
-                console.log(data);
-        })
-    } else {
-        console.log("error")
-    }
-
-    })
+function displayWeather(data) {
+    // Display temperature in Fahrenheit
 }
-
-btnEl.on('click', currentWeather);
